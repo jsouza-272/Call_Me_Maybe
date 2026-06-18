163a2c9cb8a06b6c0d67360a1d44314019d7eaf1
Joao Souza <jsouza@c1r5s9.42lisboa.com>
2026-06-18 14:12:23 +0100

    none

---
+++ Makefile
@@ -0,0 +1,2 @@
+export UV_CACHE_DIR=/goinfre/jsouza/.cache/uv
+export HF_HOME=/home/jsouza/goinfre/.cache/huggingface
--- src/__main__.py
+++ src/__main__.py
@@ -6,4 +6,26 @@ from llm_sdk import Small_LLM_Model
 flags = parsing()
 llm = Small_LLM_Model()
 
-prompt = "<| "
+functions = {}
+with open(flags['func_file']) as file:
+    functions = load(file)
+
+exemple = ('[\n{\n"prompt": "What is the sum of 2 and 3?",\n'
+           '"name": "fn_add_numbers",\n"parameters": {"a": 2.0, "b": 3.0}\n},'
+           '{\n"prompt": "Reverse the string \'hello\'",\n'
+           '"name": "fn_reverse_string",\n"parameters": {"s": "hello"}\n}\n]')
+
+system_prompt = ("<|im_start|>system"
+                 " Voce deve agir como function calling tool."
+                 "As funcoes que vc deve usar estao "
+                 f"em formato json e sao as seguintes: {functions}."
+                 "se espera uma resposta em formato de json aqui vao algums "
+                 f"exemplos de respostas esperada: {exemple}.\n"
+                 "a resposta nao deve incluir a linha de pensamento que te levou ate ela"
+                 "<|im_end|>" )
+
+prompt =  \
+<|im_start|>user \
+[Pergunta do utilizador] \
+<|im_end|> \
+<|im_start|>assistant"
